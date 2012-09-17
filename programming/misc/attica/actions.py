07d4c496803d2f5bda3b73f08d0a4c96083b382b
aydemir <yusuf.aydemir@istanbul.com>
2012-09-17 09:40:48 +0300

    attica version bump

--- programming/misc/attica/actions.py
+++ programming/misc/attica/actions.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import autotools
-from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import cmaketools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
@@ -25,4 +24,4 @@ def install():
     cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     shelltools.cd("..")
-    pisitools.dodoc("README","COPYING", "AUTHORS", "ChangeLog")
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "README")
