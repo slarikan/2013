2032e82f3272af87014f13e5f6f196cea5590d34
obsoleteman <tulliana@gmail.com>
2012-09-17 12:22:17 +0300

    update acl

--- system/base/acl/actions.py
+++ system/base/acl/actions.py
@@ -1,30 +1,31 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
     shelltools.export("OPTIMIZER", get.CFLAGS())
     shelltools.export("DEBUG", "-DNDEBUG")
 
-    autotools.rawConfigure("--libdir=/usr/lib \
+    autotools.rawConfigure("--libdir=/lib \
                             --mandir=/usr/share/man \
-                            --libexecdir=/usr/lib \
-                            --bindir=/usr/bin")
+                            --libexecdir=/lib \
+                            --bindir=/bin")
 def build():
     autotools.make()
 
 def install():
     autotools.make("DESTDIR=%s install install-lib install-dev" % get.installDIR())
 
-    pisitools.remove("/usr/lib/*.a")
+    pisitools.remove("/lib/*.a")
 
-    shelltools.chmod("%s/usr/lib/libacl.so.*.*.*" % get.installDIR(), 0755)
+    shelltools.chmod("%s/lib/libacl.so.*.*.*" % get.installDIR(), 0755)
 
     pisitools.dodoc("README")
