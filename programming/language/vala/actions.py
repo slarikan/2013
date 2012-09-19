c377d9b29baf687f46cf84c2370c41dc382cd2c9
aydemir <yusuf.aydemir@istanbul.com>
2012-09-19 12:28:49 +0300

    vala : version bump

--- programming/language/vala/actions.py
+++ programming/language/vala/actions.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2010-2011 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
@@ -17,6 +17,10 @@ def build():
     autotools.make()
 
 def check():
+    shelltools.cd("vapi")
+    shelltools.sym("glib-2.0.vapi", "GLib-2.0.vapi")
+    shelltools.sym("gobject-2.0.vapi", "GObject-2.0.vapi")
+    shelltools.cd("..")
     autotools.make("check")
 
 def install():
