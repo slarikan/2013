31ac9cf57e670cb074631db854e3302d7bbc9c7a
Erdem Artan <erdem@pardus-linux.org>
2012-09-19 21:31:08 +0300

    libusb1 is replaced by libusbx

--- system/base/libusbx/actions.py
+++ system/base/libusbx/actions.py
@@ -4,10 +4,12 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
+
+WorkDir = "libusbx-%s" % get.srcVERSION()
 
 def setup():
     options = "--disable-static"
@@ -21,6 +23,9 @@ def setup():
 
 def build():
     autotools.make()
+
+def check():
+    autotools.make("check")
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
