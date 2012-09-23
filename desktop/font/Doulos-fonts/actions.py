02c43e0c790a8931a0e614713ab572c904a749bc
Sezai Yeniay <sezaiyeniay@ozgurlukicin.com>
2012-09-23 12:47:55 +0300

    yazıtipleri güncellendi,paket sahiplikleri değiştirildi,actions.py copyright bilgisi silindi

--- desktop/font/Doulos-fonts/actions.py
+++ desktop/font/Doulos-fonts/actions.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2007-2009 TUBITAK/UEKAE
-# Licensed under the GNU General Public License, version 2.
-# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl-3.0.txt
 
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 
-WorkDir="DoulosSIL"
 
 def install():
-    shelltools.chmod("*.ttf",0644)
-    shelltools.chmod("*.txt",0644)
 
     pisitools.insinto("/usr/share/fonts/doulos","*.ttf")
 
-    pisitools.dodoc("*.txt")
+    pisitools.dodoc("*.txt","documentation/*.*")
