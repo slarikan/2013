d20ed30c06c3a605c012b4b25b46df7e15739e2d
Erdem Artan <erdem@pardus-linux.org>
2012-09-27 02:43:55 +0300

    attr

--- system/base/attr/actions.py
+++ system/base/attr/actions.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.rawConfigure("--libdir=/usr/lib \
+    autotools.rawConfigure("--libdir=/lib \
                             --mandir=/usr/share/man \
-                            --libexecdir=/usr/lib \
-                            --bindir=/usr/bin")
+                            --libexecdir=/lib \
+                            --bindir=/bin")
 def build():
     autotools.make()
 
 def install():
     autotools.make("DESTDIR=%s install install-lib install-dev" % get.installDIR())
 
-    pisitools.remove("/usr/lib/*.a")
-    shelltools.chmod("%s/usr/lib/libattr.so.*.*.*" % get.installDIR(), 0755)
-
-    pisitools.removeDir("/usr/share/doc/attr-*")
-    pisitools.dodoc("README", "doc/COPYING")
+    pisitools.remove("/lib/*.a")
+    shelltools.chmod("%s/lib/libattr.so.*.*.*" % get.installDIR(), 0755)
