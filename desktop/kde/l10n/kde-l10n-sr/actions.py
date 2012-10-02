03f6d0ae8838e641e76409e9d34e54a2dabdb611
Erdem Artan <erdem@pardus-linux.org>
2012-10-02 16:04:17 +0300

    we can't delete old copyright information

--- desktop/kde/l10n/kde-l10n-sr/actions.py
+++ desktop/kde/l10n/kde-l10n-sr/actions.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
+# Copyleft 2012 Pardus ANKA Community
 # Copyright 2008-2009 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
