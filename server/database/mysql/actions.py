7628514eeaf0ab4b589c99ff5880c8080c6b9f1f
Erdem Artan <erdem@pardus-linux.org>
2012-10-08 02:20:06 +0300

    mysql version bump, some patches and systemd service file

--- server/database/mysql/actions.py
+++ server/database/mysql/actions.py
@@ -25,39 +25,38 @@ def setup():
     shelltools.export("CFLAGS", flags)
     shelltools.export("CXXFLAGS", "%s -felide-constructors -fno-rtti -fno-exceptions" % flags)
 
-    #autotools.autoreconf("-vfi")
-
     # Configure!
     cmaketools.configure("-DCMAKE_BUILD_TYPE=Release \
-			  -DCMAKE_INSTALL_PREFIX=/usr \
-			  -DSYSCONFDIR=/etc/mysql \
-			  -DMYSQL_DATADIR=/var/lib/mysql \
-			  -DMYSQL_UNIX_ADDR=/run/mysqld/mysqld.sock \
-			  -DDEFAULT_CHARSET=utf8 \
-			  -DDEFAULT_COLLATION=utf8_general_ci \
-			  -DENABLED_LOCAL_INFILE=ON \
-			  -DINSTALL_INFODIR=share/mysql/docs \
-			  -DINSTALL_MANDIR=share/man \
-			  -DINSTALL_PLUGINDIR=lib/mysql/plugin \
-			  -DINSTALL_SCRIPTDIR=bin \
-			  -DINSTALL_INCLUDEDIR=include/mysql \
-			  -DINSTALL_DOCREADMEDIR=share/mysql \
-			  -DINSTALL_SUPPORTFILESDIR=share/mysql \
-			  -DINSTALL_MYSQLSHAREDIR=share/mysql \
-			  -DINSTALL_DOCDIR=share/mysql/docs \
-			  -DINSTALL_SHAREDIR=share/mysql \
-			  -DWITH_READLINE=ON \
-			  -DWITH_ZLIB=system \
-			  -DWITH_SSL=system \
-			  -DWITH_LIBWRAP=OFF \
-			  -DWITH_EXTRA_CHARSETS=complex \
-			  -DWITH_EMBEDDED_SERVER=ON \
-			  -DWITH_INNOBASE_STORAGE_ENGINE=1 \
-			  -DWITH_PARTITION_STORAGE_ENGINE=1 \
-			  -DWITHOUT_EXAMPLE_STORAGE_ENGINE=1 \
-			  -DWITHOUT_ARCHIVE_STORAGE_ENGINE=1 \
-			  -DWITHOUT_BLACKHOLE_STORAGE_ENGINE=1 \
-			  -DWITHOUT_FEDERATED_STORAGE_ENGINE=1 ")
+                          -DCMAKE_INSTALL_PREFIX=/usr \
+                          -DSYSCONFDIR=/etc/mysql \
+                          -DMYSQL_DATADIR=/var/lib/mysql \
+                          -DMYSQL_UNIX_ADDR=/var/run/mysqld/mysqld.sock \
+                          -DDEFAULT_CHARSET=utf8 \
+                          -DDEFAULT_COLLATION=utf8_general_ci \
+                          -DENABLED_LOCAL_INFILE=ON \
+                          -DINSTALL_INFODIR=share/info \
+                          -DINSTALL_MANDIR=share/man \
+                          -DINSTALL_PLUGINDIR=lib/mysql/plugin \
+                          -DINSTALL_SCRIPTDIR=bin \
+                          -DINSTALL_SBINDIR=sbin \
+                          -DINSTALL_INCLUDEDIR=include/mysql \
+                          -DINSTALL_DOCREADMEDIR=share/mysql \
+                          -DINSTALL_SUPPORTFILESDIR=share/mysql \
+                          -DINSTALL_MYSQLSHAREDIR=share/mysql \
+                          -DINSTALL_DOCDIR=share/mysql/docs \
+                          -DINSTALL_SHAREDIR=share/mysql \
+                          -DWITH_READLINE=ON \
+                          -DWITH_ZLIB=system \
+                          -DWITH_SSL=system \
+                          -DWITH_LIBWRAP=OFF \
+                          -DWITH_EXTRA_CHARSETS=all \
+                          -DWITH_EMBEDDED_SERVER=ON \
+                          -DWITH_INNOBASE_STORAGE_ENGINE=1 \
+                          -DWITH_PARTITION_STORAGE_ENGINE=1 \
+                          -DWITHOUT_EXAMPLE_STORAGE_ENGINE=1 \
+                          -DWITHOUT_ARCHIVE_STORAGE_ENGINE=1 \
+                          -DWITHOUT_BLACKHOLE_STORAGE_ENGINE=1 \
+                          -DWITHOUT_FEDERATED_STORAGE_ENGINE=1 ")
 
 def build():
     cmaketools.make()
@@ -75,16 +74,15 @@ def install():
     pisitools.dosym("mysqlcheck", "/usr/bin/mysqloptimize")
 
     # Cleanup
-    #pisitools.remove("/usr/share/mysql/mysql.server")
-    #pisitools.remove("/usr/share/mysql/binary-configure")
-    #pisitools.remove("/usr/share/mysql/mysql-log-rotate")
-    #pisitools.remove("/usr/share/mysql/mi_test*")
-    #pisitools.remove("/usr/share/mysql/my-*.cnf")
-    #pisitools.remove("/usr/share/mysql/config.*")
-    #pisitools.removeDir("/usr/share/aclocal")
+    pisitools.remove("/usr/share/mysql/mysql.server")
+    pisitools.remove("/usr/share/mysql/binary-configure")
+    pisitools.remove("/usr/share/mysql/mysql-log-rotate")
+    pisitools.remove("/usr/share/mysql/my-*.cnf")
+    pisitools.remove("/usr/share/mysql/config.*")
+    pisitools.removeDir("/usr/share/aclocal")
 
     # Move libs to /usr/lib
-    #pisitools.domove("/usr/lib/mysql/libmysqlclient*.so*", "/usr/lib")
+    pisitools.domove("/usr/lib/mysql/libmysqlclient*.so*", "/usr/lib")
 
     # Links to libs
     pisitools.dosym("../libmysqlclient.so", "/usr/lib/mysql/libmysqlclient.so")
@@ -94,7 +92,6 @@ def install():
     pisitools.removeDir("/usr/mysql-test")
     pisitools.removeDir("/usr/data")
     pisitools.removeDir("/usr/sql-bench")
-    #pisitools.removeDir("/usr/share/mysql/sql-bench")
     
     # Config
     pisitools.insinto("/etc/mysql", "scripts/mysqlaccess.conf")
