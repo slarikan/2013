ce9f80825d58ad955d1628420b512d0867b93e28
marcin- <marcin.bojara@gmail.com>
2012-10-11 17:18:32 +0200

    version bump

--- x11/driver/xorg-video-radeon/actions.py
+++ x11/driver/xorg-video-radeon/actions.py
@@ -4,12 +4,15 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
-from pisi.actionsapi import autotools
 from pisi.actionsapi import get
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
 
 WorkDir = "xf86-video-ati-%s" % get.srcVERSION()
 
 def setup():
+    # disable XAA to allow building against >=xorg-server-1.12.99.902
+    pisitools.dosed("configure.ac", ".*USE_XAA, 1.*")
     autotools.autoreconf("-vif")
     autotools.configure("--enable-dri")
 
@@ -18,3 +21,4 @@ def build():
 
 def install():
     autotools.install()
+    pisitools.dodoc("COPYING", "ChangeLog", "README")
