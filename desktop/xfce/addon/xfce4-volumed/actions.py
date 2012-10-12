c8d4fc4545c3ad629b56799387bfa895841e08e0
circass <erdincgultekin@gmail.com>
2012-10-12 21:50:48 +0300

    revision

--- desktop/xfce/addon/xfce4-volumed/actions.py
+++ desktop/xfce/addon/xfce4-volumed/actions.py
@@ -7,14 +7,6 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
 
-# You can use these as variables, they will replace GUI values before build.
-# Package Name : xfce4-volumed
-# Version : 0.1.13
-# Summary : A volume keys control daemon for Xfce
-
-# If the project that you are tying to compile is in a sub directory in the 
-# source archive, than you can define working directory. For example; 
-# WorkDir="xfce4-volumed-"+ get.srcVERSION() +"/sub_project_dir/"
 
 def setup():
     autotools.configure()
@@ -25,8 +17,6 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-# Take a look at the source folder for these file as documentation.
-#    pisitools.dodoc("AUTHORS", "BUGS", "ChangeLog", "COPYING", "README")
-# If there is no install rule for a runnable binary, you can 
-# install it to binary directory.
-#    pisitools.dobin("xfce4-volumed")
+
+    pisitools.dodoc("AUTHORS", "THANKS", "ChangeLog", "COPYING", "README")
+
