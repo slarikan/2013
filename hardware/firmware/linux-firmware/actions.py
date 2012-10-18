e6296d1f834db3ea63d4b104a53ad1f75ed33f11
circass <erdincgultekin@gmail.com>
2012-10-18 22:07:53 +0300

    change

--- hardware/firmware/linux-firmware/actions.py
+++ hardware/firmware/linux-firmware/actions.py
@@ -11,38 +11,26 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = "linux-firmware-20110226"
+WorkDir = "linux-firmware-20121016"
 
-# def setup():
+def setup():
     # Remove source files
-     #shelltools.unlink("usbdux/*dux")
-     #shelltools.unlink("*/*.asm")
+    shelltools.unlink("usbdux/*dux")
+    shelltools.unlink("*/*.asm")
 
     # These + a lot of other firmware are shipped within alsa-firmware
-    #for fw in ("ess", "korg", "sb16", "yamaha"):
-        #shelltools.unlinkDir(fw)
+    for fw in ("ess", "korg", "sb16", "yamaha"):
+        shelltools.unlinkDir(fw)
 
-    # This is already under v4l-dvb-firmware/
-    #shelltools.unlink("dvb-fe-xc5000-1.6.114.fw")
 
 def install():
     pisitools.insinto("/lib/firmware", "*")
 
     # Remove installed WHENCE and LIC* files from /lib/firmware
-     #pisitools.remove("/lib/firmware/GPL-3")
-     #pisitools.remove("/lib/firmware/WHENCE*")
-     #pisitools.remove("/lib/firmware/LICENCE*")
-     #pisitools.remove("/lib/firmware/LICENSE*")
-
-    # These firmwares are not needed in Pardus 2011 (2.6.36)
-    #pisitools.domove("/lib/firmware/nouveau-firmware-1/*", "/lib/firmware/nouveau")
-    #pisitools.removeDir("/lib/firmware/nouveau-firmware-1")
-
-    # Install v4l-dvb-firmware's under /lib/firmware
-     #pisitools.domove("/lib/firmware/v4l-dvb-firmware/*.fw", "/lib/firmware")
+    pisitools.remove("/lib/firmware/GPL-3")
+    pisitools.remove("/lib/firmware/WHENCE*")
+    pisitools.remove("/lib/firmware/LICENCE*")
+    pisitools.remove("/lib/firmware/LICENSE*")
 
     # Install LICENSE files
-    #pisitools.dodoc("WHENCE", "LICENCE.*", "LICENSE.*", "GPL-3")
-    #pisitools.dodoc("v4l-dvb-firmware/BOM", "v4l-dvb-firmware/LICEN*")
-
-    #pisitools.removeDir("/lib/firmware/v4l-dvb-firmware")
+    pisitools.dodoc("WHENCE", "LICENCE.*", "LICENSE.*", "GPL-3")
