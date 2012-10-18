5442e1c02fe44d9a557f7d74012567e56d0a00aa
obsoleteman <tulliana@gmail.com>
2012-10-18 14:31:45 +0300

    add emul32 support to build gnutls...

--- util/crypt/libtasn1/actions.py
+++ util/crypt/libtasn1/actions.py
@@ -6,10 +6,23 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.configure("--disable-static")
+    options = "--disable-static \
+               --enable-shared"
+
+    if get.buildTYPE() == "emul32":
+        options += " --prefix=/emul32 \
+                     --libdir=/usr/lib32 \
+                     --includedir=/usr/include"  #to have correct .pc
+        shelltools.export("CC", "%s -m32" % get.CC())
+        shelltools.export("CXX", "%s -m32" % get.CXX())
+    else:
+        options += " --libdir=/usr/lib"
+
+    autotools.configure(options)
 
     pisitools.dosed("libtool", "^sys_lib_dlsearch_path_spec=.*", "sys_lib_dlsearch_path_spec=\"/%{_lib} %{_libdir} \"")
 
@@ -21,5 +34,9 @@ def check():
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    
+    if get.buildTYPE() == "emul32":
+        pisitools.removeDir("/emul32")
+        return
 
     pisitools.dodoc("ChangeLog", "README", "NEWS", "AUTHORS", "COPYING")
