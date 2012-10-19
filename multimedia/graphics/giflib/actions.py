39d4d463ca617745186c9de8632c103000ecae6c
obsoleteman <tulliana@gmail.com>
2012-10-19 19:46:19 +0300

    revert : revert :)

--- multimedia/graphics/giflib/actions.py
+++ multimedia/graphics/giflib/actions.py
@@ -1,27 +1,39 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
+# Copyleft 2012 Pardus ANKA Community
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import libtools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
     libtools.libtoolize("--force --install")
     autotools.autoreconf("-fi")
-    autotools.configure("--with-x \
-                         --disable-gl \
-                         --disable-static")
+    
+    options = "--with-x \
+               --disable-gl \
+               --disable-static"
+
+    if get.buildTYPE() == "emul32":
+        options += " --prefix=/emul32 \
+                     --libdir=/usr/lib32"
+ 
+        shelltools.export("CFLAGS", "%s -m32" % get.CFLAGS())
+    
+    autotools.configure(options)
 
 def build():
     autotools.make("-j1")
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    if get.buildTYPE() == "emul32":
+        pisitools.removeDir("/emul32")
 
     pisitools.dohtml("doc/")
     pisitools.dodoc("AUTHORS","BUGS","ChangeLog","NEWS","doc/*.txt")
