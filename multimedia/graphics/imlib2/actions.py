00c2e7bc8d9fc9e94b9cc994e43214c30e68e5e1
namso-01 <namso-01@hotmail.it>
2012-10-19 18:02:48 +0300

    imlib2 yama eklendi ama hala paketlenmiyor.

--- multimedia/graphics/imlib2/actions.py
+++ multimedia/graphics/imlib2/actions.py
@@ -5,13 +5,14 @@
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 optimizationtype = "--enable-amd64" if get.ARCH() == "x86_64" else "--enable-mmx"
 
 def setup():
-    autotools.autoreconf("-vfi")
+    autotools.autoreconf("-fi")
     autotools.configure("--disable-static \
                          --with-x \
                          --with-jpeg \
@@ -23,6 +24,8 @@ def setup():
                          --with-id3 \
                          %s \
                          --enable-visibility-hiding" % optimizationtype)
+    shelltools.system("sed -i 's|^hardcode_libdir_flag_spec=.*|hardcode_libdir_flag_spec=""|g' libtool")
+    shelltools.system("sed -i 's|^runpath_var=LD_RUN_PATH|runpath_var=DIE_RPATH_DIE|g' libtool")
 
 def build():
     autotools.make()
