36fd19ea14b26e35ddfa434d13af05efb1556e42
marcin- <marcin.bojara@gmail.com>
2012-10-19 17:13:02 +0200

    updated, cleaned openjpeg

--- multimedia/graphics/openjpeg/actions.py
+++ multimedia/graphics/openjpeg/actions.py
@@ -6,24 +6,11 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import cmaketools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import get
 
-#WorkDir="OpenJPEG_v1_5"
-
 def setup():
-#    shelltools.system("rm -rf thirdparty")
-
-#    shelltools.makedirs("build")
-#    shelltools.cd("build")
-
-#    cmaketools.configure("-DBUILD_EXAMPLES:BOOL=ON \
-#                          -DBUILD_SHARED_LIBS:BOOL=ON \
-#                          -DINCLUDE_INSTALL_DIR=/%s/include" % get.defaultprefixDIR(), sourceDir="..")
-
     autotools.configure("--disable-static --disable-silent-rules")
 
 def build():
