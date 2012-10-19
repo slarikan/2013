54f4667d6748b71704a4b4e8e91449389681d31c
marcin- <marcin.bojara@gmail.com>
2012-10-19 17:26:11 +0200

    poppler: updated, cleaned, fixed sandbox violations

--- office/postscript/poppler/actions.py
+++ office/postscript/poppler/actions.py
@@ -6,12 +6,12 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import autotools
-from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
 
-shelltools.export("CXXFLAGS", "%s -I/usr/include/openjpeg-1.5" % get.CXXFLAGS())
+shelltools.export("HOME", get.workDIR())
 
 def setup():
     autotools.autoreconf("-fi")
