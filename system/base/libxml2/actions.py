f536153c2bf4bfaf868d134a18f9a677932a67c9
namso-01 <namso-01@hotmail.it>
2012-10-22 18:39:22 +0300

    güncelledni.

--- system/base/libxml2/actions.py
+++ system/base/libxml2/actions.py
@@ -1,25 +1,21 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-#
-# Copyleft 2012 Pardus ANKA Community
-# Copyright 2005-2011 TUBITAK/UEAKE
-# Licensed under the GNU General Public License, version 2.
-# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
-
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 from pisi.actionsapi import shelltools
 
 def setup():
-    options = "--without-threads "
+    options = "--with-zlib \
+               --with-python \
+               --with-readline \
+               --enable-ipv6 \
+               --disable-static \
+               --with-threads"
 
     if get.buildTYPE() == "emul32":
         options += " --libdir=/usr/lib32 \
                      --bindir=/emul32/bin \
                      --without-python"
         shelltools.export("CC", "%s -m32" % get.CC())
-    else: options += " --with-python"
 
     autotools.configure(options)
 
@@ -38,8 +34,8 @@ def install():
         pisitools.removeDir("/usr/share/gtk-doc")
         return
 
-    #for i in ["", "-python"]:
-        #pisitools.rename("/%s/libxml2%s-%s" % (get.docDIR(), i, get.srcVERSION()), "libxml2%s" % i)
+    for i in ["", "-python"]:
+        pisitools.rename("/%s/libxml2%s-%s" % (get.docDIR(), i, get.srcVERSION()), "libxml2%s" % i)
 
     pisitools.dodoc("AUTHORS", "ChangeLog", "NEWS", "README", "TODO")
     pisitools.removeDir("/usr/share/gtk-doc")
