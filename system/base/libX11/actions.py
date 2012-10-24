233ba486b2108465d76778eee88e30a3f0e9aa3e
circass <erdincgultekin@gmail.com>
2012-10-24 19:20:18 +0300

    Revert "open libX11 support"
    This reverts commit 823992cc24c8e27c18d0a9c5ee29318f21289751.

--- system/base/libX11/actions.py
+++ system/base/libX11/actions.py
@@ -1,34 +1,23 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyleft 2012 Pardus ANKA Community
-# Copyright 2005-2011 TUBITAK/UEAKE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
-
-shelltools.export("HOME", get.workDIR())
+from pisi.actionsapi import shelltools
 
 def setup():
     options = "--disable-static \
-               --enable-silent-rules \
-               --enable-introspection \
-               --with-libjasper \
-               --with-x11 \
-               --with-included-loaders=png"
+               --disable-specs \
+               --without-xmlto"
 
     if get.buildTYPE() == "emul32":
-        options += " --libdir=/usr/lib32 \
-                     --bindir=/emul32/bin"
-
+        options += " --libdir=/usr/lib32"
         shelltools.export("CFLAGS", "%s -m32" % get.CFLAGS())
 
-    autotools.autoreconf("-fiv")
-
+    autotools.autoreconf("-vif")
     autotools.configure(options)
 
 def build():
@@ -37,7 +26,7 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    if get.buildTYPE() == "emul32":
-        pisitools.removeDir("emul32")
-
     pisitools.dodoc("AUTHORS", "COPYING", "NEWS", "README")
+
+    if get.buildTYPE() == "emul32":
+        pisitools.removeDir("/usr/lib32/X11")
