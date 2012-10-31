ae56c63ae76d6bba11b3fc5e7e1a59d8f07cfa34
marcin- <marcin.bojara@gmail.com>
2012-10-31 23:10:17 +0100

    openjpeg added emul32

--- multimedia/graphics/openjpeg/actions.py
+++ multimedia/graphics/openjpeg/actions.py
@@ -8,15 +8,28 @@
 
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import autotools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.configure("--disable-static --disable-silent-rules")
+    options = "--disable-static --disable-silent-rules"
+
+    if get.buildTYPE() == "emul32":
+        options += "  --libdir=/usr/lib32"
+        shelltools.export("CFLAGS", "%s -m32" % get.CFLAGS())
+        shelltools.export("PKG_CONFIG_LIBDIR", "/usr/lib32/pkgconfig")
+
+    autotools.configure(options)
 
 def build():
     autotools.make()
 
 def install():
+    if get.buildTYPE() == "emul32":
+        pisitools.insinto("/usr/lib32", "libopenjpeg/.libs/libopenjpeg.so*")
+        pisitools.insinto("/usr/lib32/pkgconfig", "libopenjpeg1.pc")
+        return
+
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     pisitools.dosym("openjpeg-1.5/openjpeg.h", "/usr/include/openjpeg.h")
