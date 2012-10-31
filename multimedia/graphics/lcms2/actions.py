227a4660a56240a8f69103ada88c89e2a5b8a285
marcin- <marcin.bojara@gmail.com>
2012-10-31 22:43:14 +0100

    lcms2 version bump, added emul32

--- multimedia/graphics/lcms2/actions.py
+++ multimedia/graphics/lcms2/actions.py
@@ -6,20 +6,31 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.configure("--disable-dependency-tracking \
-                         --disable-static \
-                         --with-jpeg \
-                         --with-tiff \
-                         --with-zlib \
-                         --with-python")
+    options = "--disable-dependency-tracking \
+               --disable-static \
+               --with-jpeg \
+               --with-tiff \
+               --with-zlib \
+               --with-python"
+    if get.buildTYPE() == "emul32":
+        options += " --libdir=/usr/lib32"
+        shelltools.export("CFLAGS", "%s -m32" % get.CFLAGS())
+        shelltools.export("PKG_CONFIG_LIBDIR", "/usr/lib32/pkgconfig")
+
+    autotools.configure(options)
 
 def build():
     autotools.make()
 
 def install():
+    if get.buildTYPE() == "emul32":
+        pisitools.insinto("/usr/lib32", "src/.libs/liblcms2.so*")
+        pisitools.insinto("/usr/lib32/pkgconfig", "lcms2.pc")
+        return
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README.1ST")
