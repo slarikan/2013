f988502e3d2e3694e5f8ff1baed18db1a8b8ae78
namso-01 <namso-01@hotmail.it>
2012-11-02 23:44:24 +0200

    gtkmathview yamalandı.

--- x11/library/gtkmathview/actions.py
+++ x11/library/gtkmathview/actions.py
@@ -4,22 +4,26 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
+    # AM_BINRELOC missing, just ignore
+    shelltools.system("echo 'AC_DEFUN([AM_BINRELOC], [])' > acinclude.m4")
+    autotools.autoreconf("-fi")
     autotools.configure("--enable-popt \
-                         --enable-libxml2 \
-                         --enable-libxml2-reader \
-                         --enable-ps \
-                         --enable-tfm=2 \
-                         --enable-builder-cache \
-                         --enable-breaks \
-                         --enable-boxml \
-                         --sysconfdir=/etc \
-                         --with-t1lib \
-                         --enable-svg")
+                        --enable-libxml2 \
+                        --enable-libxml2-reader \
+                        --enable-ps \
+                        --enable-tfm=2 \
+                        --enable-builder-cache \
+                        --enable-breaks \
+                        --enable-boxml \
+                        --sysconfdir=/etc \
+                        --with-t1lib \
+                        --enable-svg")
 
 def build():
     autotools.make()
