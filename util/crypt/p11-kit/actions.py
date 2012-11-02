4e6823e2fdb8619f67eed91257ec30fc8069f44f
marcin- <marcin.bojara@gmail.com>
2012-11-02 09:28:27 +0100

    add emul32 component

--- util/crypt/p11-kit/actions.py
+++ util/crypt/p11-kit/actions.py
@@ -9,11 +9,19 @@
 from pisi.actionsapi import autotools
 from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 
 def setup():
-    autotools.configure("--prefix=/usr \
-                         --sysconfdir=/etc \
-                         --with-module-path=/usr/lib/pkcs11")
+    if get.buildTYPE() == "emul32":
+        options = " --prefix=/emul32 \
+                    --libdir=/usr/lib32 \
+                    --with-module-path=/usr/lib32/pkcs11"
+
+        shelltools.export("CC", "%s -m32" % get.CC())
+        shelltools.export("CXX", "%s -m32" % get.CXX())
+    else: options = " --with-module-path=/usr/lib/pkcs11"
+
+    autotools.configure(options)
 
 def build():
     autotools.make()
@@ -21,4 +29,7 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "README")
+    if get.buildTYPE() == "emul32":
+        pisitools.removeDir("/emul32")
+        pisitools.dosed("%s/usr/lib32/pkgconfig/p11-kit-1.pc" % get.installDIR(), "^(prefix=\/)emul32", r"\1usr")
+    else: pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "README")
