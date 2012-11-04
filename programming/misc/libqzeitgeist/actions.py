17ade2a4b4b93a37a3e424d60504ba5e1ad62934
marcin- <marcin.bojara@gmail.com>
2012-11-04 23:39:46 +0100

    libqzeitgeist restore

--- programming/misc/libqzeitgeist/actions.py
+++ programming/misc/libqzeitgeist/actions.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyleft 2012 Pardus ANKA Community
-# Copyright 2005-2011 TUBITAK/UEAKE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import autotools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import cmaketools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
+
+shelltools.export("HOME", get.workDIR())
+NoStrip=["/usr/share"]
+
 def setup():
-    autotools.configure("--disable-static")
+    cmaketools.configure()
 
 def build():
-    autotools.make("V=1")
+    cmaketools.make()
 
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("COPY*", "README", "NEWS", "AUTHORS")
-
-    pisitools.remove("/usr/share/doc/%s/INSTALL" % get.srcNAME())
+    pisitools.dodoc("COPYING", "README")
