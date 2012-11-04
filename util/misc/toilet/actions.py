127b642f8656739b8c78e4e705983a791e8dc774
namso-01 <namso-01@hotmail.it>
2012-11-04 10:08:12 +0200

    toilet'ta adam oldu.

--- util/misc/toilet/actions.py
+++ util/misc/toilet/actions.py
@@ -10,7 +10,7 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    shelltools.system("./bootstrap")
+    #shelltools.system("./bootstrap")
     autotools.configure()
 
 def build():
