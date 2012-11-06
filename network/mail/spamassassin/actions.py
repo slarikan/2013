9dc7da0c700879fea4f2b6911591ac1a4bbfec1f
marcin- <marcin.bojara@gmail.com>
2012-11-06 08:19:50 +0100

    spamassassin: we don't need manually remove '/usr/lib/perl5/vendor_perl/*/*.pod' if using perlmodules.install() and pisi release >=213

--- network/mail/spamassassin/actions.py
+++ network/mail/spamassassin/actions.py
@@ -30,7 +30,3 @@ def install():
 
     pisitools.dodoc("README", "MANIFEST", "Changes")
     pisitools.dodir("/var/lib/spamd")
-
-    pisitools.remove("/usr/lib/perl5/vendor_perl/%s/spamassassin-run.pod" % get.curPERL())
-#    pisitools.removeDir("/usr/lib/perl5/vendor_perl/%s/%s-linux-thread-multi/" % (get.curPERL(), get.ARCH()))
-
