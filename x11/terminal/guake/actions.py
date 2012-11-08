f1a991cf6bc20fade0ceb50d6fb188f85cde248d
marcin- <marcin.bojara@gmail.com>
2012-11-08 14:30:02 +0200

    guake fix sandbox violations

--- x11/terminal/guake/actions.py
+++ x11/terminal/guake/actions.py
@@ -4,13 +4,16 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
+shelltools.export("PYTHONDONTWRITEBYTECODE", "1")
+
 def setup():
     #autotools.autoreconf("-fiv")
-    autotools.configure("--disable-static ")
+    autotools.configure("--disable-static --disable-schemas-install")
 
 def build():
     autotools.make()
