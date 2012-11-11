3bbd41f0468ec7c940b20964247f4bd1f19e15de
circass <erdincgultekin@gmail.com>
2012-11-11 03:43:38 +0200

    python-mako update-fix sandbox error

--- programming/language/python/python-mako/actions.py
+++ programming/language/python/python-mako/actions.py
@@ -9,13 +9,16 @@
 from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
+
+shelltools.export("PYTHONDONTWRITEBYTECODE", "1")
 
 WorkDir = "Mako-%s" % get.srcVERSION()
 
 def install():
     pythonmodules.install()
 
-    pisitools.dodoc("doc/*.txt")
+    #pisitools.dodoc("doc/*.txt")
     pisitools.dohtml("doc/*.html")
     pisitools.insinto("%s/%s" % (get.docDIR(), get.srcNAME()), "doc/build")
     pisitools.insinto("%s/%s" % (get.docDIR(), get.srcNAME()), "examples")
