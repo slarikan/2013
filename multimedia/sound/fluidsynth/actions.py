b9eb45cb2cc16b026157d08f1c069cf084276fd7
marcin- <marcin.bojara@gmail.com>
2012-11-13 20:30:14 +0100

    fluidsynth fix build

--- multimedia/sound/fluidsynth/actions.py
+++ multimedia/sound/fluidsynth/actions.py
@@ -7,12 +7,15 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
 from pisi.actionsapi import cmaketools
-from pisi.actionsapi import autotools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
+shelltools.export("CFLAGS", "%s -lpthread" % get.CFLAGS())
+
 def setup():
-    cmaketools.configure("-Denable-ladspa=on . -DLIB_SUFFIX=\"\"")
+    cmaketools.configure("-Denable-ladspa=ON \
+                          -DLIB_SUFFIX=\"\"")
 
 def build():
     cmaketools.make()
