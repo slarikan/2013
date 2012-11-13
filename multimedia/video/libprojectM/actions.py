5187575762370cf41fc37f0b1db0a41e95ae46b7
marcin- <marcin.bojara@gmail.com>
2012-11-13 17:48:22 +0100

    libprojectM fix build

--- multimedia/video/libprojectM/actions.py
+++ multimedia/video/libprojectM/actions.py
@@ -4,11 +4,13 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import cmaketools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 #WorkDir = "projectM-%s-Source" % get.srcVERSION()
+shelltools.export("CXXFLAGS", "%s -lGL" % get.CXXFLAGS())
 
 def setup():
     cmaketools.configure()
