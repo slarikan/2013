afd814c70bd31b8c53bab4bd2b2d8c75df383556
marcin- <marcin.bojara@gmail.com>
2012-11-14 13:05:41 +0100

    mate-doc-utils remove coflicetd files with gnome-doc-utils

--- desktop/mate/base/mate-doc-utils/actions.py
+++ desktop/mate/base/mate-doc-utils/actions.py
@@ -12,11 +12,12 @@ from pisi.actionsapi import get
 
 def setup():
     shelltools.copy("tools/mate-doc-utils.m4", "m4/")
-    shelltools.system("./autogen.sh --prefix=/usr \
-			--disable-scrollkeeper \
-			--sysconfdir=/etc \
-			--mandir=/usr/share/man \
-			--localstatedir=/var")
+    shelltools.system("./autogen.sh \
+                       --prefix=/usr \
+                       --disable-scrollkeeper \
+                       --sysconfdir=/etc \
+                       --mandir=/usr/share/man \
+                       --localstatedir=/var")
     autotools.configure()
 
 def build():
@@ -25,4 +26,11 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR()) 
     pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "INSTALL", "NEWS", "README")
-    shelltools.unlink(get.installDIR() + "/usr/share/mate-doc-utils/mate-debian.sh")
+    pisitools.remove("/usr/share/mate-doc-utils/mate-debian.sh")
+
+    # remove conflicted files with gnome-doc-utils
+    pisitools.removeDir("usr/lib/python2.7/site-packages/xml2po")
+    pisitools.remove("usr/bin/xml2po")
+    pisitools.remove("usr/share/man/man1/xml2po.1")
+    pisitools.remove("usr/share/pkgconfig/xml2po.pc")
+    pisitools.removeDir("usr/share/xml/mallard")
