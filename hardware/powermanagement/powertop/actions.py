e07ee88a7545b522ace51a11af13c6933de39e99
namso-01 <namso-01@hotmail.it>
2012-11-19 23:14:30 +0200

    powertop güncellenfi.

--- hardware/powermanagement/powertop/actions.py
+++ hardware/powermanagement/powertop/actions.py
@@ -10,6 +10,9 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
+def setup():
+    autotools.configure()
+
 def build():
     autotools.make()
 
