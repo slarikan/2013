90e2bb1c0c2bfa77e6f6d85e6cfa705f69bc51f5
namso-01 <namso-01@hotmail.it>
2012-11-20 22:52:48 +0200

    iscan güncellendi.

--- hardware/scanner/iscan/actions.py
+++ hardware/scanner/iscan/actions.py
@@ -15,14 +15,15 @@ iscan_data = "iscan-data-1.8.1"
 
 def setup():
     # Setup iscan-data
-    shelltools.cd(iscan_data)
-    autotools.configure()
-    shelltools.cd("..")
+    #shelltools.cd(iscan_data)
+    #autotools.configure()
+    #shelltools.cd("..")
 
-    shelltools.unlink("m4/libtool.m4")
+    #shelltools.unlink("m4/libtool.m4")
 
-    shelltools.export("AUTOPOINT", "true")
-    autotools.autoreconf("-fi")
+    #shelltools.export("AUTOPOINT", "true")
+    shelltools.export("LDFLAGS", "-ldl -lpng15")
+    autotools.autoreconf("-vif")
 
     autotools.configure("--disable-static \
                          --enable-frontend \
@@ -34,23 +35,23 @@ def setup():
                          --disable-rpath")
 
 def build():
-    autotools.make("-C %s" % iscan_data)
+    #autotools.make("-C %s" % iscan_data)
     autotools.make()
-    autotools.make("-C po update-po")
+    #autotools.make("-C po update-po")
 
 def install():
     autotools.install()
-    autotools.install("-C %s" % iscan_data)
+    #autotools.install("-C %s" % iscan_data)
 
     # Remove unused stuff from iscan-data
-    pisitools.remove("/usr/share/iscan-data/sled10*")
-    pisitools.remove("/usr/share/iscan-data/fdi.xsl")
+    #pisitools.remove("/usr/share/iscan-data/sled10*")
+    #pisitools.remove("/usr/share/iscan-data/fdi.xsl")
 
     # Install sane backend configuration file
     pisitools.insinto("/etc/sane.d", "backend/epkowa.conf")
 
     # Install documentation
-    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README", "%s/SUPPORTED-DEVICES" % iscan_data)
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README")
 
     # Needed for iscan-registry
     pisitools.dodir("/var/lib/iscan")
