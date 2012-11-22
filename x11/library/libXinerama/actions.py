27b5bbd1725827bd8c851ae4c9d952cb58053cb3
marcin- <marcin.bojara@gmail.com>
2012-11-22 15:19:30 +0100

    libXinerama version bump

--- x11/library/libXinerama/actions.py
+++ x11/library/libXinerama/actions.py
@@ -9,13 +9,7 @@ from pisi.actionsapi import get
 from pisi.actionsapi import shelltools
 
 def setup():
-    options = "--disable-static"
-
-    if get.buildTYPE() == "emul32":
-        options += " --libdir=/usr/lib32"
-        shelltools.export("CFLAGS", "%s -m32" % get.CFLAGS())
-
-    autotools.configure(options)
+    autotools.configure("--disable-static")
 
 def build():
     autotools.make()
