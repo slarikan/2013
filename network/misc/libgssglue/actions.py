d8ac41c8073026677cea0d61c67976b5618bbed0
marcin- <marcin.bojara@gmail.com>
2012-11-22 22:06:04 +0100

    libgssglue add emul32 component

--- network/misc/libgssglue/actions.py
+++ network/misc/libgssglue/actions.py
@@ -6,6 +6,7 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import get
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
@@ -20,6 +21,6 @@ def check():
     autotools.make("check")
 
 def install():
-    autotools.install()
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     pisitools.dodoc("ChangeLog", "AUTHORS", "README", "NEWS")
