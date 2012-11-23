ead0c81c4b00ec4dd1953c463f36b7a04e3253c0
namso-01 <namso-01@hotmail.it>
2012-11-23 17:01:59 +0200

    programming/misc/libgdata/ güncellendi.

--- programming/misc/libgdata/actions.py
+++ programming/misc/libgdata/actions.py
@@ -21,7 +21,7 @@ def build():
     autotools.make()
 
 def install():
-    autotools.install()
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README")
 
