52a09da8061833d0c74f6f9d6b2ffc0f2878ca63
canatan <qazsew@mynet.com>
2012-11-24 14:38:14 +0200

    NOT

--- network/mail/exim/actions.py
+++ network/mail/exim/actions.py
@@ -8,15 +8,15 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 from pisi.actionsapi import shelltools
-
-shelltools.export("USER","q")
+import subprocess
 
 def setup():
     shutil.copy("/var/pisi/exim-4.80.1-1/work/exim-4.80.1/src/EDITME","/var/pisi/exim-4.80.1-1/work/exim-4.80.1/Local/Makefile")
     shutil.copy("/var/pisi/exim-4.80.1-1/work/exim-4.80.1/exim_monitor/EDITME","/var/pisi/exim-4.80.1-1/work/exim-4.80.1/Local/eximon.conf")   
     pisitools.dosed("/var/pisi/exim-4.80.1-1/work/exim-4.80.1/Local/Makefile","/usr/exim/bin","/usr/bin")
     pisitools.dosed("/var/pisi/exim-4.80.1-1/work/exim-4.80.1/Local/Makefile","/usr/exim/configure","/etc/exim.conf")
-    pisitools.dosed("/var/pisi/exim-4.80.1-1/work/exim-4.80.1/src/buildconfig.c","EXIM_USER","USER")
+    pisitools.dosed("/var/pisi/exim-4.80.1-1/work/exim-4.80.1/Local/Makefile","EXIM_USER=","EXIM_USER=%s" % subprocess.check_output("",0,"/usr/bin/users").strip())
+    pisitools.rename("/usr/bin/exim-4.80.1-3","exim")
     pisitools.dosed("/var/pisi/exim-4.80.1-1/work/exim-4.80.1/Local/Makefile","/var/spool/exim","/var/pisi/exim-4.80.1-1/install/var/spool/exim")
     
 def build():
