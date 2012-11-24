00c6f299ce7dec8006200309d59d5dae262db288
marcin- <marcin.bojara@gmail.com>
2012-11-24 23:57:44 +0100

    miscfiles fix build

--- system/base/miscfiles/actions.py
+++ system/base/miscfiles/actions.py
@@ -10,10 +10,18 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
+dirs = ("dict", "rfc")
+
 def setup():
-    autotools.configure()
+    autotools.configure("--datadir=/usr/share/misc")
 
 def install():
-    autotools.rawInstall("prefix=%s/usr" % get.installDIR())
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    for d in dirs:
+        pisitools.dodir("/usr/share/%s" % d)
+        for line in tuple(open("Makefile.am", 'r')):
+            if line.startswith(d):
+                for f in line.strip().split("=")[1][1:].split(" "):
+                    pisitools.domove("/usr/share/misc/%s" % f, "/usr/share/%s/" % d)
 
     pisitools.dodoc("GNU*", "NEWS", "ORIGIN", "README", "dict-README")
