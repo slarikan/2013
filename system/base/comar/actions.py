6add225c89b9615202f33d15596fb72ca8587a9d
marcin- <marcin.bojara@gmail.com>
2012-11-25 01:22:00 +0100

    comar fix build when python3 is installed

--- system/base/comar/actions.py
+++ system/base/comar/actions.py
@@ -13,7 +13,8 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    cmaketools.configure()
+    cmaketools.configure("-DPYTHON_INCLUDE_DIR=/usr/include/python2.7 \
+                          -DPYTHON_LIBRARY=/usr/lib/libpython2.7.so")
 
 def build():
     autotools.make("VERBOSE=1")
