f093ecc94fa75e8d7c794be232ce9251b012b54e
canatan <qazsew@mynet.com>
2012-11-26 21:16:04 +0200

    bits fix

--- network/mail/qpopper/actions.py
+++ network/mail/qpopper/actions.py
@@ -4,12 +4,17 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import get
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 WorkDir="qpopper4.1.0"
+cfg=""
+if get.ARCH() == "x86_64":
+    cfg="--enable-64-bit "
+        
 
 def setup():
-    autotools.configure("--enable-64-bit --enable-spool-dir=/var/spool/clientmqueue")
+    autotools.configure("%s--enable-spool-dir=/var/spool/clientmqueue" % cfg)
 
 def build():
     autotools.make()
