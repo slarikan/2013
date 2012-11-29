5f68ae2a4fb286a18c19cfec4afcea431ca71da2
canatan <qazsew@mynet.com>
2012-11-29 17:10:11 +0200

    bazı özellikler açıldı

--- network/mail/qpopper/actions.py
+++ network/mail/qpopper/actions.py
@@ -7,19 +7,29 @@
 from pisi.actionsapi import get
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+import subprocess
+
 WorkDir="qpopper4.1.0"
 cfg=""
 if get.ARCH() == "x86_64":
     cfg="--enable-64-bit "
+else:
+    cfg="--enable-32-bit "
         
 
 def setup():
-    autotools.configure("%s--enable-spool-dir=/var/spool/clientmqueue" % cfg)
+    pisitools.dosed("./popper/Makefile.in","\\$\\{installdir\\}/\\$\\{pop_auth\\} -init -safe;","")
+    autotools.configure("%s--enable-spool-dir=/var/spool/mail --enable-servermode --enable-specialauth --enable-apop=/etc/pop.auth --enable-log-login \
+			 --enable-server-mode-group-include=users --enable-poppassd --enable-popuid=%s --with-openssl=/usr/lib/openssl --with-gdbm=/usr/lib" % (cfg,subprocess.check_output("",0,"/usr/bin/users").strip())) 
+
+		#--enable-bulldb=/var/spool/bulls,--with-kerberos5=/usr/lib >>missing header(<et/com_err.h>) ve undefined reference(db.h'ta tanımlı) veriyor.biara fixlerim.
 
 def build():
     autotools.make()
 
 def install():
+    pisitools.dodir("/usr/bin")
+    pisitools.dodir("/usr/sbin")
     autotools.install()
 
     #pisitools.removeDir("/usr/share")
