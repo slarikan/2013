c2092413895ce4238fd5965f980ec6d3cffe06ab
obsoleteman <tulliana@gmail.com>
2012-12-05 04:43:39 +0200

    geany : fix geany executabe installation and sandbox

--- editor/geany/actions.py
+++ editor/geany/actions.py
@@ -3,6 +3,8 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
 
 def setup():
 	autotools.configure()
@@ -14,5 +16,5 @@ def install():
 	pisitools.dodoc('AUTHORS', 'ChangeLog', 'ChangeLog.pre-0-17',
 	                'COPYING', 'HACKING', 'INSTALL', 'NEWS', 'README',
 	                'README.I18N', 'README.Packagers', 'THANKS', 'TODO')
-	autotools.install()
+	autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
