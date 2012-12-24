479d67fa16a9356c001c20f38ab573940211f0a2
aydemir <yusuf.aydemir@istanbul.com>
2012-12-24 10:40:43 +0200

    rocs . some fixed actions

--- desktop/kde/base/kdeedu/rocs/actions.py
+++ desktop/kde/base/kdeedu/rocs/actions.py
@@ -21,4 +21,4 @@ def build():
 def install():
     kde4.install()
 
-    pisitools.dodoc("AUTHORS", "CHANGELOG", "COPYING*", "NEWS", "README", "TODO")
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING*", "NEWS", "README")
