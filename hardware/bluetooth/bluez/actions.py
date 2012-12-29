04891fd03bd8dc4e75a318de2dc49f87da009e1c
namso-01 <namso-01@hotmail.it>
2012-12-30 00:05:59 +0200

    bluez güncellendi.

--- hardware/bluetooth/bluez/actions.py
+++ hardware/bluetooth/bluez/actions.py
@@ -31,9 +31,10 @@ def setup():
                          --enable-pand \
                          --enable-test \
                          --enable-pcmcia \
+                         --disable-systemd \
                          --with-systemdunitdir=/lib/systemd/system \
                          --with-ouifile=/usr/share/misc/oui.txt \
-                         --disable-hid2hci \
+                         --enable-hid2hci \
                          --libexecdir=/lib")
 def build():
     autotools.make()
@@ -42,23 +43,23 @@ def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     # Install conf files
-    for i in ["audio", "input", "network"]:
-        pisitools.insinto("/etc/bluetooth", "%s/%s.conf" % (i,i))
+    for i in ["audio", "input", "network", "proximity"]:
+        pisitools.insinto("/etc/bluetooth", "profiles/%s/%s.conf" % (i,i))
+        
+    pisitools.insinto("/etc/bluetooth", "src/bluetooth.conf")
+    pisitools.insinto("/etc/bluetooth", "src/main.conf")
 
     # Simple test tools
-    for i in ["test-adapter", "test-audio", "test-discovery",
-              "test-health-sink", "test-manager", "test-network",
-              "test-proximity", "test-serial", "test-service",
-              "test-attrib", "test-device", "test-health", "test-input",
-              "test-nap", "test-oob", "test-sap-server", "test-serial-proxy",
-              "test-telephony", "test-thermometer", "monitor-bluetooth",
-              "simple-agent", "simple-endpoint", "simple-player",
-              "simple-service", "sap-client", "list-devices", "hsplay", "hsmicro"]:
+    for i in ["test-adapter", "test-discovery", "test-health-sink",
+               "test-manager", "test-network", "test-proximity", "test-device",
+               "test-health", "test-nap", "test-sap-server", "test-thermometer",
+               "monitor-bluetooth", "simple-agent", "simple-endpoint", "simple-player",
+              "simple-service", "list-devices"]:
         pisitools.dobin("test/%s" % i)
 
     # Additional tools
-    pisitools.dosbin("tools/hcisecfilter")
-    pisitools.dosbin("tools/ppporc")
+    #pisitools.dosbin("tools/hcisecfilter")
+    #pisitools.dosbin("tools/ppporc")
 
     # Install documents
     pisitools.dodoc("AUTHORS", "ChangeLog", "README")
