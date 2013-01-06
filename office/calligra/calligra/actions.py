010e310d4a93d2c3829a2fd549e1971112e00a4e
Anıl Özbek <ozbekanil@gmail.com>
2013-01-06 13:46:39 +0200

    calligra: some cosmetic fixes

--- office/calligra/calligra/actions.py
+++ office/calligra/calligra/actions.py
@@ -3,11 +3,12 @@
 #
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
-import os
-from pisi.actionsapi import kde4
-from pisi.actionsapi import pisitools
+
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import kde4
 from pisi.actionsapi import get
+import os
 
 shelltools.export("HOME", get.workDIR())
 
