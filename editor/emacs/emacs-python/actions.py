02ec78448c0977600a37662aaca71828208d2be2
namso-01 <namso-01@hotmail.it>
2013-01-09 21:56:42 +0200

    emacs-python güncellendi.

--- editor/emacs/emacs-python/actions.py
+++ editor/emacs/emacs-python/actions.py
@@ -7,7 +7,7 @@
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = "python-mode.el-6.0.11"
+#WorkDir = "python-mode.el-6.0.11"
 
 def install():
     pisitools.insinto("/usr/share/emacs/site-lisp/python", "*.el", "python-mode.el")
