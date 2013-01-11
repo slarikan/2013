475c602e866cce966a3d219816cf54fda26c2f57
obsoleteman <tulliana@gmail.com>
2013-01-11 18:28:06 +0200

    polkit: remove comar script, use baselayout setings

--- system/base/polkit/comar/package.py
+++
@@ -1,7 +0,0 @@
-#/usr/bin/python
-
-from pisi.util import run_batch
-
-def postInstall(fromVersion, fromRelease, toVersion, toRelease):
-    if not run_batch("cat /etc/passwd | grep polkitd")[1].split(":")[5] == "/var/lib/polkit-1":
-        run_batch("usermod -d /var/lib/polkit-1 polkitd")
