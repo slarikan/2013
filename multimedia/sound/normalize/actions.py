e9a2e4620539c42658089405456aaf23e22b65c8
marcin- <marcin.bojara@gmail.com>
2013-01-14 12:03:39 +0100

    normalize: fix build

--- multimedia/sound/normalize/actions.py
+++ multimedia/sound/normalize/actions.py
@@ -6,11 +6,15 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import libtools
 from pisi.actionsapi import get
 
 def setup():
+    for f in ("AUTHORS", "ChangeLog"): shelltools.touch(f)
+    autotools.autoreconf("-fiv")
     autotools.configure("--with-mad \
                          --disable-xmms \
                          --with-audiofile")
