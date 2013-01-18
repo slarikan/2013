030358b27a094941b2dab94e328f583a4f00514c
Ertan Güven <guven.ertan@gmail.com>
2013-01-19 00:45:09 +0200

    Update desktop/enlightenment/addon/e-module-exebuf/actions.py

--- desktop/enlightenment/addon/e-module-exebuf/actions.py
+++ desktop/enlightenment/addon/e-module-exebuf/actions.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2010 TUBITAK/BILGEM
+# Copyleft 2012 Pardus ANKA Community
+# Copyright 2005-2011 TUBITAK/UEAKE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
