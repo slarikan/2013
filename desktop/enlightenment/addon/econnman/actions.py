a2d8c76289f46089d0b1a64e90f536c928045797
Ertan Güven <guven.ertan@gmail.com>
2013-01-19 00:52:57 +0200

    Update desktop/enlightenment/addon/econnman/actions.py

--- desktop/enlightenment/addon/econnman/actions.py
+++ desktop/enlightenment/addon/econnman/actions.py
@@ -1,8 +1,9 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/copyleft/gpl.txt
+# Copyleft 2012 Pardus ANKA Community
+# Copyright 2005-2011 TUBITAK/UEAKE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import get
 from pisi.actionsapi import autotools
