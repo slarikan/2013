a5850b2c138012c67caae2fd2e0e511cbc49bb39
Serdar Soytetir <tulliana@gmail.com>
2013-01-19 23:36:48 +0200

    Update desktop/font/bdf-unifont/actions.py

--- desktop/font/bdf-unifont/actions.py
+++ desktop/font/bdf-unifont/actions.py
@@ -5,12 +5,9 @@
 # See the file http://www.gnu.org/licenses/gpl-3.0.txt
 
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import get
 
 WorkDir="."
 
 def install():
-    #shelltools.system("gunzip %s" % pkg)
     pisitools.insinto("/usr/share/fonts/misc/","*.bdf")
 
