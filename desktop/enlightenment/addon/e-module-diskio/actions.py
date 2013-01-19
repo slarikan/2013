fa591dea491f999210e0b14d93f575d2a3139774
circass <erdincgultekin@gmail.com>
2013-01-19 02:12:26 +0200

    fix licence misunderstanding

--- desktop/enlightenment/addon/e-module-diskio/actions.py
+++ desktop/enlightenment/addon/e-module-diskio/actions.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 #
-# Copyleft 2012 Pardus ANKA Community
-# Copyright 2005-2011 TUBITAK/UEAKE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
--- desktop/enlightenment/addon/e-module-everything-places/actions.py
+++ desktop/enlightenment/addon/e-module-everything-places/actions.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # Copyleft 2012 Pardus ANKA Community
-# Copyright 2005-2011 TUBITAK/UEAKE
+# Copyright 2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
