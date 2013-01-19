9f6099d630e57531ef1c7e11356823ce1374113a
Ertan Güven <guven.ertan@gmail.com>
2013-01-19 17:16:23 +0200

    playonlinux acions.py edit

--- hardware/emulator/playonlinux/actions.py
+++ hardware/emulator/playonlinux/actions.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright © 2008  TUBITAK/UEKAE
+# Copyleft 2012 Pardus ANKA Community
+# Copyright 2006-2011 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
-# See the file http://www.gnu.org/copyleft/gpl.txt.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
