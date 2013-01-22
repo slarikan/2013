4a547c6a244ba08bef747ac78a533430673d4b2f
marcin- <marcin.bojara@gmail.com>
2013-01-22 23:54:08 +0100

    harfbuzz: add emul32

--- desktop/font/harfbuzz/actions.py
+++ desktop/font/harfbuzz/actions.py
@@ -3,11 +3,15 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
-from pisi.actionsapi import autotools
 from pisi.actionsapi import get
+from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 
 def setup():
+    if get.buildTYPE() == "emul32":
+        shelltools.system('patch -p0 -i "../remove-graphite.patch"')
+        autotools.autoreconf("-fiv")
     autotools.configure("--disable-static")
 
 def build():
