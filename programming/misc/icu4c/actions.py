7964054e13e644f88031f4677ec0ec9f4f0afcb9
marcin- <marcin.bojara@gmail.com>
2013-01-22 23:28:04 +0100

    icu4c: fix emul32 build

--- programming/misc/icu4c/actions.py
+++ programming/misc/icu4c/actions.py
@@ -5,19 +5,24 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import autotools
 from pisi.actionsapi import get
+from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 
 WorkDir="icu/source"
 
 def setup():
     autotools.autoconf("-f")
-    options = "--with-data-packaging=library --disable-samples"
+    options = "--with-data-packaging=library \
+               --disable-samples \
+               --disable-silent-rules"
     if get.buildTYPE() == "_emul32":
         options += " --libdir=/usr/lib32 \
                      --bindir=/_emul32/bin \
                      --sbindir=/_emul32/sbin"
+        shelltools.export("CC", "%s -m32" % get.CC())
+        shelltools.export("CXX", "%s -m32" % get.CXX())
     autotools.configure(options)
     pisitools.dosed("config/mh-linux", "-nodefaultlibs -nostdlib")
 
@@ -28,7 +33,7 @@ def check():
     autotools.make("check")
 
 def install():
-    autotools.rawInstall('DESTDIR="%s"' % get.installDIR())
+    autotools.rawInstall('-j1 DESTDIR="%s"' % get.installDIR())
     if get.buildTYPE() == "_emul32":
         pisitools.domove("/_emul32/bin/icu-config", "/usr/bin", "icu-config-32")
         pisitools.removeDir("/_emul32")
