6c87907b8fae63715d8ae1843ac4ee1b895c8e08
marcin- <marcin.bojara@gmail.com>
2013-01-23 12:36:26 +0100

    libspectre: add emul32

--- programming/misc/libspectre/actions.py
+++ programming/misc/libspectre/actions.py
@@ -6,19 +6,19 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import get
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
     autotools.configure("--disable-static \
                          --enable-test \
-                         --disable-dependency-tracking \
-                         --with-gnu-ld")
+                         --disable-dependency-tracking")
 
 def build():
     autotools.make()
 
 def install():
-    autotools.install()
+    autotools.rawInstall('DESTDIR="%s"' % get.installDIR())
 
     pisitools.dodoc("ChangeLog","COPYING","NEWS","README")
